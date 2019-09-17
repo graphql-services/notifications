f8a73e21a8b3dd0eae1ba79926887791b47e8e38
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-09-17 16:35:04 +0200

    Add client

---
+++ client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/graphql-services/go-saga/graphqlorm"
+	"github.com/graphql-services/notifications/gen"
+)
+
+// Client ...
+type Client struct {
+	gc *graphqlorm.ORMClient
+}
+
+// NewClient ...
+func NewClient(URL string) *Client {
+	client := graphqlorm.NewClient(URL)
+	return &Client{client}
+}
+
+type Notification gen.Notification
+
+type CreateNotificationInput struct {
+	Message     string    `json:"message"`
+	Principal   *string   `json:"principal"`
+	Channel     *string   `json:"channel"`
+	Reference   *string   `json:"reference"`
+	ReferenceID *string   `json:"referenceID"`
+	Date        time.Time `json:"date"`
+}
+
+// InviteUser invite user with given Email. If user with given email exists, it just return without any invitation.
+func (c *Client) CreateNotification(ctx context.Context, input CreateNotificationInput) (result graphqlorm.MutationResult, err error) {
+	return c.gc.CreateEntity(ctx, graphqlorm.CreateEntityOptions{
+		Entity: "Notification",
+		Input:  input,
+	})
+}
--- go.mod
+++ go.mod
@@ -7,7 +7,10 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gofrs/uuid v3.2.0+incompatible
 	github.com/graph-gophers/dataloader v5.0.0+incompatible
+	github.com/graphql-services/go-saga v0.0.0-20190917142920-46ab02eae4de
 	github.com/jinzhu/gorm v1.9.10
+	github.com/machinebox/graphql v0.2.2
+	github.com/matryer/is v1.2.0 // indirect
 	github.com/mitchellh/mapstructure v0.0.0-20180203102830-a4e142e9c047
 	github.com/novacloudcz/graphql-orm v0.0.0-20190912114504-1f558d910934
 	github.com/rs/cors v1.6.0
